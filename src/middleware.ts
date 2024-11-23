2cfbcaacb7de3a51c33a071ec4bcb35bda2f18da
Nicholas Oxford <maheledaily@gmail.com>
2024-11-23 09:59:52 -0700

    add routes to ignore middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,9 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Exclude static files and images
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    // Exclude static files, images, and public routes
+    "/((?!_next/static|_next/image|favicon.ico|docs|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    // Exclude home page
+    "/((?!^$).*)",
   ],
 };
