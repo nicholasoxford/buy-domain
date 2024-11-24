a921dccf16a5643f1fd2ca41d5125d09132234f1
Nicholas Oxford <maheledaily@gmail.com>
2024-11-24 10:24:31 -0700

    fix sign out

--- src/app/(main)/auth/signout/route.ts
+++ src/app/(main)/auth/signout/route.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@/lib/supabase/server";
-import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 export async function POST() {
   const supabase = await createClient();
   await supabase.auth.signOut();
-
-  return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL));
+  redirect("/");
 }
