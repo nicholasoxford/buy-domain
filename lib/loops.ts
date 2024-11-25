2cd95161521fdcd84a374f8d18cf809610d83f9c
Nicholas Oxford <maheledaily@gmail.com>
2024-11-25 10:50:04 -0700

    try catch loops email

--- lib/loops.ts
+++ lib/loops.ts
@@ -1,14 +1,12 @@
 import { LoopsClient } from "loops";
 
-const LOOPS_API_KEY = process.env.LOOPS_API_KEY ?? "";
-const loops = new LoopsClient(LOOPS_API_KEY);
-
-export default loops;
-
 export async function sendDomainAddedNotification(
   domain: string,
   email: string
 ) {
+  const LOOPS_API_KEY = process.env.LOOPS_API_KEY ?? "";
+  const loops = new LoopsClient(LOOPS_API_KEY);
+  console.log("Loops API Key: ", LOOPS_API_KEY);
   try {
     console.log(
       "Sending domain added notification to: ",
