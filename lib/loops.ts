c30a9091fc24def624cdcb5b3fd39d02d8eb9982
Nicholas Oxford <maheledaily@gmail.com>
2024-11-25 10:36:40 -0700

    try catch loops email

--- lib/loops.ts
+++ lib/loops.ts
@@ -9,16 +9,20 @@ export async function sendDomainAddedNotification(
   domain: string,
   email: string
 ) {
-  console.log("Sending domain added notification to: ", email);
-  const resp = await loops.sendTransactionalEmail({
-    transactionalId: "cm3wis2ev00o3epr4uf1yrq5g",
-    email,
-    dataVariables: {
-      domain,
-    },
-  });
-  if (!resp.success) {
-    throw new Error("Failed to send domain added notification");
+  try {
+    console.log("Sending domain added notification to: ", email);
+    const resp = await loops.sendTransactionalEmail({
+      transactionalId: "cm3wis2ev00o3epr4uf1yrq5g",
+      email,
+      dataVariables: {
+        domain,
+      },
+    });
+    if (!resp.success) {
+      throw new Error("Failed to send domain added notification");
+    }
+    console.log("Domain added notification sent successfully to: ", email);
+  } catch (error) {
+    console.error("Error sending domain added notification: ", error);
   }
-  console.log("Domain added notification sent successfully to: ", email);
 }
