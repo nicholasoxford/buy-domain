f9726efd1d4b8c5138ff04ae1073d4ee8ce66817
Nicholas Oxford <maheledaily@gmail.com>
2024-11-24 21:09:07 -0700

    home page?

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import { HomePage } from "@/components/home-page";
 import { createClient } from "@/lib/supabase/server";
 
+export const dynamic = "force-static";
+
 export default async function Home() {
   const supabase = await createClient();
   const { data: user } = await supabase.auth.getUser();
