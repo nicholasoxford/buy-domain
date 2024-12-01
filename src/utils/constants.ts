70b730c055a0e66b9a57196827b84d50b619825d
Nicholas Oxford <maheledaily@gmail.com>
2024-11-30 17:21:17 -0700

    constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 export const BUY_TEMPLATE_STRIPE_LINK =
-  "https://buy.stripe.com/eVadTW5D04udfjqdQQ";
+  "https://pay.dash.domains/b/eVadTW5D04udfjqdQQ";
 
 export const BUY_BASIC_DOMAIN_BRIDGE_SUBSCRIPTION_LINK =
-  "https://buy.stripe.com/test_5kA6rg5j2bdt6ekfYZ";
+  "https://pay.dash.domains/b/3cs4jm4yW0dX2wE6op";
 
 export const BUY_PRO_DOMAIN_BRIDGE_SUBSCRIPTION_LINK =
-  "https://buy.stripe.com/eVaaH898181hbyE144";
+  "https://pay.dash.domains/b/00gcPS5D02m5c7e7sv";
 
 export const ADD_DOMAIN_BUY_BASIC_DOMAIN_BRIDGE_SUBSCRIPTION_LINK =
-  "https://buy.stripe.com/test_fZeg1Q4eY2GX9qwdQS";
+  "https://pay.dash.domains/b/eVa9DG6H47Gp5IQ4gi";
 
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!;
